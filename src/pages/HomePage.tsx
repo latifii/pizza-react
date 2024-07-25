19a6625f261f9f478216e51aa6e0163786984736
latifii <latifii68@gmail.com>
2024-07-25 09:05:16 +0330

    add tailwind

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 function HomePage() {
   return (
     <div>
-      <h1>
+      <h1 className="m-3 bg-slate-400 py-2 text-right text-xl font-bold text-blue-800">
         The best pizza.
         <br />
         Straight out of the oven, straight to you.
